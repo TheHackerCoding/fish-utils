8141ed7d1694c5bd48ac27368ebdd1b035d63966
TheHackerCoding <triniziah@gmail.com>
2022-04-22 00:52:24 -0400

    small additions

--- src/abbrs.fish
+++ src/abbrs.fish
@@ -1,3 +1,4 @@
 #!/usr/local/bin/fish
 abbr -a install 'sudo apt install'
 abbr -a installdeb 'sudo dpkg -i'
+abbr -a remove 'sudo apt remove'
--- src/functions.fish
+++ src/functions.fish
@@ -19,3 +19,20 @@ function gcommit
   git commit -m $argv
   git push
 end
+
+function addpaths
+    contains -- $argv $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $argv
+    echo "Updated PATH: $PATH"
+end
+
+function removepath
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated PATH: $PATH"
+    else
+        echo "$argv[1] not found in PATH: $PATH"
+    end
+end
+
+# credit: https://superuser.com/a/1212305
