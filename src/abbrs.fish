25b4e6600ec8e29849eec411929435eb53ce69ed
TheHackerCoding <triniziah@gmail.com>
2022-06-07 18:38:16 -0400

    add some new stuff

--- src/abbrs.fish
+++ src/abbrs.fish
@@ -2,3 +2,7 @@
 abbr -a install 'sudo apt install'
 abbr -a installdeb 'sudo dpkg -i'
 abbr -a remove 'sudo apt remove'
+abbr -a push 'git push'
+abbr -a pull 'git pull'
+abbr -a commit 'git commit'
+abbr -a gadd 'git add .'
--- src/main.fish
+++ src/main.fish
@@ -3,4 +3,5 @@ set ttmp "$HOME/.config/fish/fish-utils"
 source $ttmp/src/alias.fish
 source $ttmp/src/functions.fish
 source $ttmp/src/abbrs.fish
+source $ttmp/src/vars.fish
 set -e ttmp
